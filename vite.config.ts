e86efb14547ab09b6c648134393608ad3ee08ebd
254PINCHEZ <githinjipeter001@gmail.com>
2026-01-14 16:12:48 +0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,20 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // Make sure this is '/'
+  base: '/',
   server: {
     port: 5173,
     host: true,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    }
   }
 })
